29f17586ddbcf2d2240b7dfa89a80111c67bcb20
Mwia <iansnr10@gmail.com>
2025-10-02 09:15:14 +0300

    creating a function to display age in months in the dom

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+function ageFromYearsToMonths(){
+    const yourYears = document.getElementById('age').value;
+    const totalMonths = Number(yourYears) * 12
+    return totalMonths
+    
+}
+function showAgeInMonths(){
+    const ageInMonths = document.getElementById('age-in-months').textContent = `Your age in months is ${ageFromYearsToMonths()}`
+    return ageInMonths
+}
+document.getElementById('form').addEventListener('submit', function(stopReload){
+    stopReload.preventDefault()
+    showAgeInMonths()
+});
